0b401a89a1223cbe60dc8e0e1ace6f9bf6175eee
ramesh <ramesh_pokhrel@lftechnology.com>
2024-02-14 23:38:16 +0545

    Feat: use replica for server

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const appid= process.env.APPID
 
 app.get('/', function (req, res) {
-  res.send(`${appid}: listening on port ${appid} `);
+  res.send(`Rep ?? called ${appid}: listening on port ${appid} `);
 });
 app.listen(appid, function () {
   console.log(`Example app listening on port ${appid}!`);
